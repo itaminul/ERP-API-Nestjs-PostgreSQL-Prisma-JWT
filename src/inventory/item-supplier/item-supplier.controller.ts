9e6fb1c2ef3608303656606aea324858fac837d9
itaminul <itaminul@gmail.com>
2023-08-21 22:51:25 +0600

    get all item

--- src/inventory/item-supplier/item-supplier.controller.ts
+++ src/inventory/item-supplier/item-supplier.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import { ItemSupplierService } from './item-supplier.service';
+import { Prisma, Users } from '@prisma/client';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthUserInfo } from 'src/decorator/auth.user.info.decorator';
 
 @Controller('item-supplier')
-export class ItemSupplierController {}
+export class ItemSupplierController {
+    constructor(private readonly itemSupplierService: ItemSupplierService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get()
+    async getAll(@AuthUserInfo() authUserInfo: Users){
+        try {
+            const response = await this.itemSupplierService.getAll(authUserInfo)
+            return { message: "Show Successfully", status: HttpStatus.OK, response }
+          } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      
+            }
+            throw error
+          }
+    }
+}
