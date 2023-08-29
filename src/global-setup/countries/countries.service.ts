876a4abf4cd52e4580a3ab0fad68fc7720eab30e
itaminul@gmail.com <itaminul@gmail.com>
2023-08-29 14:37:49 +0600

    add global setup

---
+++ src/global-setup/countries/countries.service.ts
@@ -0,0 +1,60 @@
+import { Body, Injectable, Param } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { CreateCountriesDto } from './dto/create.countries.dto';
+import { UpdateCountriesDto } from './dto/update.countries.dto';
+
+@Injectable()
+export class CountriesService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAll(authUserInfo) {
+        return await this.prisma.country.findMany({
+            where: {
+                activeStatus: true
+            }
+        })
+    }
+
+    async create(@Body() createCountriesDto: CreateCountriesDto, authUserInfo) {
+        const {
+            countryName,
+            countryDescription,
+            countryCode
+        } = createCountriesDto
+        await this.prisma.country.create({
+            data: {
+                countryName,
+                countryDescription,
+                countryCode,
+                createdBy: authUserInfo.id,
+                createdDate: new Date().toLocaleDateString(),
+                createdTime: new Date().toLocaleTimeString(),
+                createdAt: new Date()
+            }
+        })
+    }
+
+    async update(@Param('id') id: number, @Body() updateCountriesDto: UpdateCountriesDto, authUserInfo) {
+        const {
+            countryName,
+            countryDescription,
+            countryCode,
+            activeStatus
+        } = updateCountriesDto
+        await this.prisma.country.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                countryName,
+                countryDescription,
+                countryCode,
+                activeStatus,
+                createdBy: authUserInfo.id,
+                createdDate: new Date().toLocaleDateString(),
+                createdTime: new Date().toLocaleTimeString(),
+                createdAt: new Date()
+            }
+        })
+    }
+}
---
+++ src/global-setup/countries/dto/create.countries.dto.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+@Injectable()
+export class CreateCountriesDto {
+    @IsString()
+    countryName: string
+    @IsOptional()
+    @IsString()
+    countryDescription: string
+    @IsOptional()
+    @IsNumber()
+    countryCode: number
+}
