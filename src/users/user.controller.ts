58d8cdd6d926e8012dbe045cb48567bb0711001a
itaminul@gmail.com <itaminul@gmail.com>
2023-09-03 15:15:55 +0600

    get active and inactive user

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,32 +1,57 @@
 import { Body, Controller, Get, HttpException, HttpStatus, Post, Request, UseGuards } from '@nestjs/common'
 import { UserService } from './user.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Users } from '@prisma/client';
 import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { compare } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthUserInfo } from 'src/decorator/auth.user.info.decorator';
 
 @Controller('user')
 export class UserController{
-constructor(
-    private readonly  userService: UserService,
-    private jwtService: JwtService,
-    private prisma: PrismaService,
-    private readonly configService: ConfigService
-    ){}
+    constructor(
+        private readonly  userService: UserService,
+        private jwtService: JwtService,
+        private prisma: PrismaService,
+        private readonly configService: ConfigService
+        ){}
 
-      
-@Get('/getAllUser')
-async getAllUser() {
+
+    @UseGuards(AuthGuard('jwt'))      
+    @Get('/getAllUser')
+    async getAllUser(@AuthUserInfo() authUserInfo: Users) {
+      try {
+        const getTotalUser = await this.userService.getAllTotalUser(authUserInfo)
+        const results = await this.userService.getAllUser(authUserInfo)
+        return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, getTotalUser }
+    } catch (error) {
+      return { success: false, message: error.message }
+  }
+}
+
+@UseGuards(AuthGuard('jwt'))      
+@Get('/getAllActiveUsers')
+async getAllActiveUsers(@AuthUserInfo() authUserInfo: Users) {
   try {
-    const response = await this.userService.getAllUser()
-    return { message: "Show successfully", status: HttpStatus.OK, response}
+    const totalActiveUser = await this.userService.getAllTotalActiveUsers(authUserInfo)
+    const results = await this.userService.getAllActiveUsers(authUserInfo)
+    return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, totalActiveUser }
 } catch (error) {
-    if(error instanceof Prisma.PrismaClientKnownRequestError){
+  return { success: false, message: error.message }
+}
+}
 
-    }
-    throw error;
+@UseGuards(AuthGuard('jwt'))      
+@Get('/getAllInActiveUsers')
+async getAllInActiveUsers(@AuthUserInfo() authUserInfo: Users) {
+  try {
+    const totalInactiveUser = await this.userService.getAllTotalInActiveUsers(authUserInfo)
+    const results = await this.userService.getAllInActiveUsers(authUserInfo)
+    return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, totalInactiveUser }
+} catch (error) {
+  return { success: false, message: error.message }
 }
 }
 
@@ -34,39 +59,38 @@ async getAllUser() {
 @Post('/register')
 async signUp(@Body() body: any) {
     try {
-        const response = await this.userService.signUp(body)
-        return { message: "User created successfully", status: HttpStatus.OK, response}
+        const results = await this.userService.signUp(body)
+        return { message: 'User Created Successfully', success: true, status: HttpStatus.OK, results }
     } catch (error) {
-        if(error instanceof Prisma.PrismaClientKnownRequestError){
-
-        }
-        throw error;
-    }
+      return { success: false, message: error.message }
+  }
 }
 
 // @UseGuards(LocalAuthGuard)
 @Post('/login')
 async login(@Body() body:any, @Request() req) {
     const userInfo = {Users:req.session}
+    console.log("body", body)
     // return { Users: req.user}
     // console.log("session", userInfo)
     const checkUserExists = await this.prisma.users.findFirst({
         where: {
-          username: body.username,
-        },
-      });
-      
-      if (!checkUserExists) {
+          username: body.usernmae,
+          activeStatus: true
+      },
+  });
+    
+    if (!checkUserExists) {
         throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-      }
-      
-      const checkPassword = await compare(
+    }
+    
+    const checkPassword = await compare(
         body.password,
         checkUserExists.password,
-      );
-              
-     delete checkUserExists.password;
-      if (checkPassword) {
+        );
+    
+    delete checkUserExists.password;
+    if (checkPassword) {
         const accessToken = this.generateJWT({
           sub: checkUserExists.id,
           username: checkUserExists.username,
@@ -76,33 +100,33 @@ async login(@Body() body:any, @Request() req) {
           desigId: checkUserExists.desigId,
           roleId: checkUserExists.roleId,
           orgId:checkUserExists.orgId,
-        });
-          
+      });
+        
         return {
           statusCode: 200,
           message: 'Login Successfully',
           accessToken: accessToken,
         //   userInfo:userInfo
-        };
-      } else {
-        throw new HttpException(
-          'User or password not match',
-          HttpStatus.UNAUTHORIZED,
-        );
-      }
-      
+      };
+  } else {
+    throw new HttpException(
+      'User or password not match',
+      HttpStatus.UNAUTHORIZED,
+      );
 }
-        generateJWT(payload: any) {
-            return this.jwtService.sign(payload, {
-            secret: this.configService.get('JWT_SECRET'),
-            expiresIn: this.configService.get('expired'),
-            });
-        }
 
-        @Get('/logout')
-        async logout(@Request() req){
-            req.session.destroy();
-            return { msg: 'The user session has ended' }
-            }
+}
+generateJWT(payload: any) {
+    return this.jwtService.sign(payload, {
+        secret: this.configService.get('JWT_SECRET'),
+        expiresIn: this.configService.get('expired'),
+    });
+}
+
+@Get('/logout')
+async logout(@Request() req){
+    req.session.destroy();
+    return { msg: 'The user session has ended' }
+}
 
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -4,24 +4,81 @@ import * as bcrypt from 'bcrypt'
 @Injectable()
 export class UserService{
     constructor(private readonly prisma: PrismaService){}
-    async getAllUser() {
+
+    async getAllUser(authUserInfo) {
         return await this.prisma.users.findMany()
     }
+
+    async getAllTotalUser(authUserInfo) {
+        return await this.prisma.users.aggregate({
+            _count: {
+                id: true
+            }
+        })
+    }
+
+    async getAllActiveUsers(authUserInfo) {
+        return await this.prisma.users.findMany({
+            where: {
+                activeStatus: true,
+                orgId: authUserInfo.orgId
+            }
+        })
+    }
+
+    async getAllTotalActiveUsers(authUserInfo) {
+        return await this.prisma.users.aggregate({
+            _count: {
+                id: true
+            },
+            where: {
+                activeStatus: true
+            }
+        })
+    }
+
+    async getAllInActiveUsers(authUserInfo) {
+        return await this.prisma.users.findMany({
+            where: {
+                activeStatus: false,
+                orgId: authUserInfo.orgId
+            }
+        })
+    }
+
+    async getAllTotalInActiveUsers(authUserInfo) {
+        return await this.prisma.users.aggregate({
+            _count: {
+                id: true
+            },
+            where: {
+                activeStatus: false
+            }
+        })
+        
+    }
+
     async signUp(@Body() body: any) {
-        const{ username, password} = body
+        const{ username, password,mobileNumber,emailAddress,deptId, desigId,roleId,orgId,companyId} = body
         const saltOrRounds = 10;
         const hashedPasword = await bcrypt.hash(password, saltOrRounds)        
         const data = await this.prisma.users.create({
             data: {
                 username: username,
                 password: hashedPasword,
+                mobileNumber,
+                emailAddress,
+                deptId,
+                desigId,
+                roleId,
+                orgId,
+                companyId:1,
                 createdDate: new Date().toLocaleDateString(),
                 createdTime: new Date().toLocaleTimeString()
 
             }
         })
     }
-    
 
     async getUserByuserName(username: string) {
         const data = await this.prisma.users.findMany({
