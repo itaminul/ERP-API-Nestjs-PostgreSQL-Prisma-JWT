b6cdf2ffda8e5fd9c1a7195872eba3e5958335b2
itaminul <itaminul@gmail.com>
2023-09-08 14:59:55 +0600

    create exam setup service

--- src/quiz-test/setup/exam/exam-setup/exam-setup.service.ts
+++ src/quiz-test/setup/exam/exam-setup/exam-setup.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { CreateExamSetupDto } from './dto/create-exam-setup.dto';
 
 @Injectable()
-export class ExamSetupService {}
+export class ExamSetupService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAll() {
+        retun this.prisma.examSetup.getAll()
+    }
+
+    async create(@Body() dto: CreateExamSetupDto) {
+        const { examName, examDescription } = dto
+        this.prisma.examSetup.create({
+            data: {
+                examName,
+                examDescription,
+                createdDate: new Date().toLocaleDateString(),
+                createdTime: new Date().toLocaleTimeString(),
+                createdAt: new Date()
+            }
+        })
+    }
+}
