e9ddbeed8c47b4121a2dc1ab41f78da7a4b51b59
itaminul <itaminul@gmail.com>
2023-09-11 22:34:25 +0600

    update exam setup
    update exam setup

--- src/quiz-test/setup/exam/exam-setup/exam-setup.controller.ts
+++ src/quiz-test/setup/exam/exam-setup/exam-setup.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, HttpStatus } from '@nestjs/common';
+import { Body, Controller, HttpStatus } from '@nestjs/common';
 import { ExamSetupService } from './exam-setup.service';
+import { CreateExamSetupDto } from './dto/create-exam-setup.dto';
 
 @Controller('exam-setup')
 export class ExamSetupController {
@@ -8,6 +9,14 @@ export class ExamSetupController {
         try {
             const results = await this.examService.getAll()            
             return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results }
+        } catch (error) {
+            return { success: false, message: error.message }
+        }
+    }
+    async create(@Body() dto: CreateExamSetupDto) {
+        try {
+            const results = await this.examService.create()            
+            return { message: 'Insert Successfully', success: true, status: HttpStatus.OK, results }
         } catch (error) {
             return { success: false, message: error.message }
         }
