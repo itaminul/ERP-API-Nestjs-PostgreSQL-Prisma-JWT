90b8aec9096fa795875bfe8a770470ad466aba3a
itaminul <itaminul@gmail.com>
2023-12-07 18:44:43 +0600

    attendance service with code formate

--- src/hr/movement/dto/create.movement.dto.ts
+++ src/hr/movement/dto/create.movement.dto.ts
@@ -1,34 +1,34 @@
-import { Injectable } from "@nestjs/common";
-import { IsNumber, IsOptional, IsString } from "class-validator";
+import { Injectable } from '@nestjs/common';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 @Injectable()
 export class CreateMovementDto {
-    @IsNumber()
-    empId: number
-    @IsOptional()
-    @IsNumber()
-    clientId: number
-    @IsOptional()
-    @IsNumber()
-    projectId: number
-    @IsNumber()
-    movementStatus: number
-    @IsString()
-    movementReasons: string
-    @IsOptional()
-    @IsString()
-    movementDes: string
-    @IsString()
-    movementFromDate: string
-    @IsString()
-    movementToDate: string
-    @IsOptional()
-    @IsString()
-    movementFromTime: string
-    @IsOptional()
-    @IsString()
-    movementToTime: string
-    @IsOptional()
-    @IsString()
-    emergencyContact: string
+  @IsNumber()
+  empId: number;
+  @IsOptional()
+  @IsNumber()
+  clientId: number;
+  @IsOptional()
+  @IsNumber()
+  projectId: number;
+  @IsNumber()
+  movementStatus: number;
+  @IsString()
+  movementReasons: string;
+  @IsOptional()
+  @IsString()
+  movementDes: string;
+  @IsString()
+  movementFromDate: string;
+  @IsString()
+  movementToDate: string;
+  @IsOptional()
+  @IsString()
+  movementFromTime: string;
+  @IsOptional()
+  @IsString()
+  movementToTime: string;
+  @IsOptional()
+  @IsString()
+  emergencyContact: string;
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Request, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { Prisma, Users } from '@prisma/client';
-import { LocalAuthGuard } from 'src/auth/local.auth.guard';
+import { Users } from '@prisma/client';
+// import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { compare } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthUserInfo } from 'src/decorator/auth.user.info.decorator';
-
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -16,19 +25,24 @@ export class UserController {
     private readonly userService: UserService,
     private jwtService: JwtService,
     private prisma: PrismaService,
-    private readonly configService: ConfigService
-  ) { }
-
+    private readonly configService: ConfigService,
+  ) {}
 
   @UseGuards(AuthGuard('jwt'))
   @Get('/getAllUser')
-  async getAllUser(@AuthUserInfo() authUserInfo: Users) {
+  async getAllUser() {
     try {
-      const getTotalUser = await this.userService.getAllTotalUser(authUserInfo)
-      const results = await this.userService.getAllUser(authUserInfo)
-      return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, getTotalUser }
+      const getTotalUser = await this.userService.getAllTotalUser();
+      const results = await this.userService.getAllUser();
+      return {
+        message: 'Show Successfully',
+        success: true,
+        status: HttpStatus.OK,
+        results,
+        getTotalUser,
+      };
     } catch (error) {
-      return { success: false, message: error.message }
+      return { success: false, message: error.message };
     }
   }
 
@@ -36,11 +50,17 @@ export class UserController {
   @Get('/getAllActiveUsers')
   async getAllActiveUsers(@AuthUserInfo() authUserInfo: Users) {
     try {
-      const totalActiveUser = await this.userService.getAllTotalActiveUsers(authUserInfo)
-      const results = await this.userService.getAllActiveUsers(authUserInfo)
-      return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, totalActiveUser }
+      const totalActiveUser = await this.userService.getAllTotalActiveUsers();
+      const results = await this.userService.getAllActiveUsers(authUserInfo);
+      return {
+        message: 'Show Successfully',
+        success: true,
+        status: HttpStatus.OK,
+        results,
+        totalActiveUser,
+      };
     } catch (error) {
-      return { success: false, message: error.message }
+      return { success: false, message: error.message };
     }
   }
 
@@ -48,47 +68,52 @@ export class UserController {
   @Get('/getAllInActiveUsers')
   async getAllInActiveUsers(@AuthUserInfo() authUserInfo: Users) {
     try {
-      const totalInactiveUser = await this.userService.getAllTotalInActiveUsers(authUserInfo)
-      const results = await this.userService.getAllInActiveUsers(authUserInfo)
-      return { message: 'Show Successfully', success: true, status: HttpStatus.OK, results, totalInactiveUser }
+      const totalInactiveUser =
+        await this.userService.getAllTotalInActiveUsers();
+      const results = await this.userService.getAllInActiveUsers(authUserInfo);
+      return {
+        message: 'Show Successfully',
+        success: true,
+        status: HttpStatus.OK,
+        results,
+        totalInactiveUser,
+      };
     } catch (error) {
-      return { success: false, message: error.message }
+      return { success: false, message: error.message };
     }
   }
-
 
   @Post('/register')
   async signUp(@Body() body: any) {
     try {
-      const results = await this.userService.signUp(body)
-      return { message: 'User Created Successfully', success: true, status: HttpStatus.OK, results }
+      const results = await this.userService.signUp(body);
+      return {
+        message: 'User Created Successfully',
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
     } catch (error) {
-      return { success: false, message: error.message }
+      return { success: false, message: error.message };
     }
   }
 
   // @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Body() body: any, @Request() req) {
-    const userInfo = { Users: req.session }
-    console.log("body", body)
-    // return { Users: req.user}
-    // console.log("session", userInfo)
-    const checkUserExists = await this.prisma.users.findFirst({
+  async login(@Body() dto: LoginUserDto) {
+    const checkUserExists = await this.prisma.users.findUnique({
       where: {
-        username: body.usernmae,
-        activeStatus: true
+        username: dto.username,
+        activeStatus: true,
       },
     });
+    console.log('checkUserExists', checkUserExists);
 
     if (!checkUserExists) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    const checkPassword = await compare(
-      body.password,
-      checkUserExists.password,
-    );
+    const checkPassword = await compare(dto.password, checkUserExists.password);
 
     delete checkUserExists.password;
     if (checkPassword) {
@@ -103,7 +128,6 @@ export class UserController {
         orgId: checkUserExists.orgId,
       });
 
-
       return {
         statusCode: 200,
         message: 'Login Successfully',
@@ -116,7 +140,6 @@ export class UserController {
         HttpStatus.UNAUTHORIZED,
       );
     }
-
   }
   generateJWT(payload: any) {
     return this.jwtService.sign(payload, {
@@ -128,7 +151,6 @@ export class UserController {
   @Get('/logout')
   async logout(@Request() req) {
     req.session.destroy();
-    return { msg: 'The user session has ended' }
+    return { msg: 'The user session has ended' };
   }
-
 }
