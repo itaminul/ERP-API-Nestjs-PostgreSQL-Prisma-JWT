eea9a4a00276f74b38c1fbef7da77d5aee17e309
aminul@atilimited.net <aminul@atilimited.net>
2024-03-28 17:53:29 +0600

    product info

--- src/ecommerce/product-info/product-info.controller.ts
+++ src/ecommerce/product-info/product-info.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
+import { ProductInfoService } from './product-info.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Users } from '@prisma/client';
 
 @Controller('product-info')
-export class ProductInfoController {}
+export class ProductInfoController {
+    constructor(public readonly productInfoService: ProductInfoService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get()
+    async getAll(authUserInfo: Users) {
+        try {
+            const results = await this.productInfoService.getAll(authUserInfo);
+            return {
+              message: 'Show data successfully',
+              success: true,
+              status: HttpStatus.OK,
+              results,
+            };
+          } catch (error) {
+            return { success: false, message: error.message };
+          }
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('/:id')
+    async getProductById(@Param('id') id: number, authUserInfo: Users) {
+        try {
+            const results = await this.productInfoService.getAllProductById(id, authUserInfo);
+            return {
+              message: 'Show data successfully',
+              success: true,
+              status: HttpStatus.OK,
+              results,
+            };
+          } catch (error) {
+            return { success: false, message: error.message };
+          }
+    }
+}
--- src/ecommerce/product-info/product-info.service.ts
+++ src/ecommerce/product-info/product-info.service.ts
@@ -1,4 +1,4 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Body, Injectable, Param } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { CreateProductSetupDto } from './dto/create.product.setup.dto';
 
@@ -6,9 +6,18 @@ import { CreateProductSetupDto } from './dto/create.product.setup.dto';
 export class ProductInfoService {
     constructor(public readonly prisma: PrismaService) {}
 
-    async getAll() {
+    async getAll(authUserInfo) {
         return await this.prisma.invItemSetup.findMany({
             where: {
+                activeStatus: true
+            }
+        })
+    }
+
+    async getAllProductById(@Param('id') id: number, authUserInfo) {
+        return await this.prisma.invItemSetup.findUnique({
+            where: {
+                id: Number(id),
                 activeStatus: true
             }
         })
