cc1f335bfc471bb81c4d07e61b990d3f0153583c
itaminul <itaminul@gmail.com>
2024-04-03 19:32:07 +0600

    order start

--- src/modules/ecommerce.module.ts
+++ src/modules/ecommerce.module.ts
@@ -1,18 +1,24 @@
 import { Module } from "@nestjs/common";
+import { OrderController } from "src/ecommerce/order/order.controller";
+import { OrderModule } from "src/ecommerce/order/order.module";
+import { OrderService } from "src/ecommerce/order/order.service";
 import { ProductInfoController } from "src/ecommerce/product-info/product-info.controller";
 import { ProductInfoModule } from "src/ecommerce/product-info/product-info.module";
 import { ProductInfoService } from "src/ecommerce/product-info/product-info.service";
 
 @Module({
   imports: [
-    ProductInfoModule
+    ProductInfoModule,
+    OrderModule
   ],
   controllers: [
-    ProductInfoController
+    ProductInfoController,
+    OrderController
   ],
   providers: [
-    ProductInfoService
+    ProductInfoService,
+    OrderService
   ]
 })
 
-export class ProductInformationModule{}
+export class EcommerceModule{}
