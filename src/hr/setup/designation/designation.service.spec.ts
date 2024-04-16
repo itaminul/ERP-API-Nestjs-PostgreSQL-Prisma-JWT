164618500fc5b5762a282cb1e666407ca0a7da8f
itaminul@gmail.com <itaminul@gmail.com>
2024-04-16 12:02:45 +0600

    unit testing of designation update service

--- src/hr/setup/designation/designation.service.spec.ts
+++ src/hr/setup/designation/designation.service.spec.ts
@@ -14,7 +14,7 @@ const MockPrismaService = {
     create: jest.fn(),
     findUnique: jest.fn(),
     update: jest.fn(),
-    patch: jest.fn()
+    patch: jest.fn(),
   },
 };
 
@@ -70,40 +70,60 @@ describe('DesignationService', () => {
           designationDes: createDesignationDto.designationDes,
           orgId: createDesignationDto.orgId,
           serialNo: createDesignationDto.serialNo,
-          updated: createDesignationDto.createdBy,
+          createdBy: createDesignationDto.createdBy,
         }),
       });
     });
   });
 
-  describe('patch', () => {
+  describe('update', () => {
     it('should update a designation', async () => {
       // Mock data
       const authUserInfo = mockAuthUserInfo;
       const designationId = 1;
       const updateDesignationDto = new UpdateDesignatinDto(); // Instantiate the DTO
-      updateDesignationDto.designationName = 'Updated Manager';
-      updateDesignationDto.designationDes = 'Updated Manages the team';
-      updateDesignationDto.orgId = 2; // Example value
-      updateDesignationDto.serialNo = 2; // Example value
-      updateDesignationDto.activeStatus = true; // Example value
+      updateDesignationDto.designationName =
+        updateDesignationDto.designationName;
+      updateDesignationDto.designationDes = updateDesignationDto.designationDes;
+      updateDesignationDto.orgId = updateDesignationDto.orgId;
+      updateDesignationDto.serialNo = updateDesignationDto.serialNo;
+      updateDesignationDto.activeStatus = true;
       updateDesignationDto.updatedBy = authUserInfo.id;
-  
+
       const existingDesignation = {
         id: designationId,
-        designationName: 'Manager',
-        designationDes: 'Manages the team',
-        orgId: 1,
+        designationName: updateDesignationDto.designationName,
+        designationDes: updateDesignationDto.designationDes,
+        orgId: updateDesignationDto.orgId,
         activeStatus: true,
-        updatedBy: authUserInfo.id
+        updatedBy: authUserInfo.id,
       };
-  
+
+      const updatedDesignation = {
+        ...existingDesignation,
+        designationName: updateDesignationDto.designationName,
+        designationDes: updateDesignationDto.designationDes,
+        orgId: updateDesignationDto.orgId,
+        serialNo: updateDesignationDto.serialNo,
+        activeStatus: updateDesignationDto.activeStatus,
+        updatedBy: updateDesignationDto.updatedBy,
+      };
+
       // Mock PrismaService's findUnique method to return existing designation
-      MockPrismaService.designation.findUnique.mockResolvedValue(existingDesignation);
-  
+      MockPrismaService.designation.findUnique.mockResolvedValue(
+        existingDesignation,
+      );
+      // Mock PrismaService's update method to return the updated designation
+      MockPrismaService.designation.update.mockResolvedValue(
+        updatedDesignation,
+      );
+
       // Call the update method
-      const result = await service.update(designationId, updateDesignationDto, mockAuthUserInfo);
-      console.log("result", result);
+      const result = await service.update(
+        designationId,
+        updateDesignationDto,
+        mockAuthUserInfo,
+      );
       expect(MockPrismaService.designation.update).toHaveBeenCalledWith({
         where: { id: designationId },
         data: expect.objectContaining({
@@ -112,14 +132,12 @@ describe('DesignationService', () => {
           orgId: updateDesignationDto.orgId,
           serialNo: updateDesignationDto.serialNo,
           activeStatus: true,
-          createdBy: 1
+          updatedBy: 1,
         }),
       });
-  
+
       // Verify that the result is as expected
-      expect(result).toEqual(updateDesignationDto);
+      expect(result).toEqual(updatedDesignation);
     });
   });
-  
-  
 });
